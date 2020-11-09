75def822f47b3be61eb029715eda4b28c3b04b13
Joao Zsigmond <jzsiggy@gmail.com>
2020-11-09 12:33:16 -0300

    Adding routes to index

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,18 @@
 require('dotenv').config()
 const express = require('express');
+const session = require('express-session');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+
+app.use(session({
+    secret: 'keyboard cat',
+    saveUninitialized: true,
+    resave: false,
+    credentials: 'same-origin',
+    cookie: { secure: false , maxAge: 60000 }
+}))
 
 app.use(bodyParser.urlencoded({ extended : false }));
 app.use(bodyParser.json());
@@ -11,10 +20,16 @@ app.use(bodyParser.json());
 app.use(cors({
     origin : ['http://localhost:3000'],
     credentials : true,
-  }));
+}));
 
-const { movieRouter } = require('./routes');
+app.use((req, res, next) => {
+    console.log(req.session);
+    next()
+})
+
+const { movieRouter , authRouter} = require('./routes');
 app.use('/movies/', movieRouter);
+app.use('/auth/', authRouter);
 
 app.listen(5000, () => {
     console.log(
--- backend/src/controller/index.js
+++ backend/src/controller/index.js
@@ -1,7 +1,21 @@
 const {
-    getRandomMovie
+    getRandomMovie,
+    getPlaylist,
 } = require('./movie')
 
+const {
+    login,
+    logout,
+    signup,
+    getCurrentUser
+} = require('./auth')
+
 module.exports = {
-    getRandomMovie
+    login,
+    logout,
+    signup,
+    getCurrentUser,
+    
+    getRandomMovie,
+    getPlaylist
 }
